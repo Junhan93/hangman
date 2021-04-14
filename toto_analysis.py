0d971251489d5f7a9310a53a9e8faf0216f80cfe
Junhan93 <junhan.3008@gmail.com>
2021-04-14 18:49:50 +0800

    added import module toto_algo

--- toto_analysis.py
+++ toto_analysis.py
@@ -1,15 +1,7 @@
 
+import toto_algorithm as tta
 import random
 
-toto_numbers = [
-    1,2,3,4,5,6,7,
-    8,9,10,11,12,13,14,
-    15,16,17,18,19,20,21,
-    22,23,24,25,26,27,28,
-    29,30,31,32,33,34,35,
-    36,37,38,39,40,41,42,
-    43,44,45,46,47,48,49
-]
 winning_numbers = []
 analysis = []
 
@@ -28,8 +20,8 @@ tally = []
 def generate_sets(iter):
     count = 0
     for x in range(iter):
-        random.shuffle(toto_numbers)
-        winning_numbers = toto_numbers[:6]
+        random.shuffle(tta.toto_numbers)
+        winning_numbers = tta.toto_numbers[:6]
         analysis.append(winning_numbers)
 
     return analysis
